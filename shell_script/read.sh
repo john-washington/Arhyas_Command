7c544a2b3b267d03858dbdf1371a4b57f4b60d92
john-washington <52287450+john-washington@users.noreply.github.com>
2025-09-19 07:29:52 +0800

    Update read.sh
    commented out unnecessary code so it runs on macOS and android

--- shell_script/read.sh
+++ shell_script/read.sh
@@ -7,20 +7,20 @@ str_to_hex() {
     printf '%s' "$1" | od -An -t x1 | tr -d ' \n'
 }
 
-timeout 15 ping -c 1 $host 
-status=$?
+#timeout 15 ping -c 1 $host 
+#status=$?
 
-if [ $status -eq 124 ]; then
-        echo "$host is probably unpingable..."
-elif [ $status -ne 0 ]; then
-	echo "command failed with status: $status"
-else
+#if [ $status -eq 124 ]; then
+#        echo "$host is probably unpingable..."
+#elif [ $status -ne 0 ]; then
+#	echo "command failed with status: $status"
+#else
   while IFS=, read -r field1 field2
   do
-    sleep 30
+    sleep 3
     echo "Field 1: $field1"
     hex_string=$(str_to_hex "$field1")
     ping -c 1 -p "$hex_string" $host
   done < "$csv_file"
-fi
+#fi
 exit 0
