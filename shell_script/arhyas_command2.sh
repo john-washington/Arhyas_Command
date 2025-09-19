eb2f43d357574d4752b2cd05802abdcf9027ac62
john-washington <52287450+john-washington@users.noreply.github.com>
2025-09-19 14:09:31 +0800

    Update arhyas_command2.sh
    skipping the home and provider gateways to speed it up

--- shell_script/arhyas_command2.sh
+++ shell_script/arhyas_command2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
  
-
+counter=0
 traceroute "$1" | while IFS=  read -r line;
 do
 	echo "processing line: $line"
-	for word in $line; do
+        if [ $counter -gt 2 ]; then
+	  for word in $line; do
 		if [[ $word =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 			echo "Found IP address: $word"
 			echo "_ELAi_sa_sequence..."
@@ -20,6 +21,7 @@ do
 			echo "_Arhyas_Command_sequence..."
 			sh read.sh _Arhyas_Command_sequence.csv "$word"
 		fi
-	done
+	 done
+	fi
+	((counter++))
 done 
-#"$trace_file"
