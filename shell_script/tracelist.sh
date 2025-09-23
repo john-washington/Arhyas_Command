a5bf629e9c29918ccdd40c7a094d8e7ba3f19adb
john-washington <52287450+john-washington@users.noreply.github.com>
2025-09-23 17:14:00 +0800

    Update tracelist.sh
    skipping my gateway

--- shell_script/tracelist.sh
+++ shell_script/tracelist.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-	
+
+count=0
 while IFS=  read -r line; do
-	#echo "processing line: $line"
-	for word in $line; do
-		if [[ $word =~ ^[a-zA-Z|0-9]{1,3}\.[a-zA-Z|0-9]{1,3}\.[a-zA-Z|0-9]{1,3}(\.[0-9]{1,3})?$ ]]; then
-			echo "$word"
-		fi
-	done
+  #echo "processing: $line"
+  if [[ $count -gt 1 ]]; then
+    for word in $line; do
+      if [[ $word =~ ^[a-zA-Z|0-9]{1,3}\.[a-zA-Z|0-9]{1,3}\.[a-zA-Z|0-9]{1,3}(\.[0-9]{1,3})?$ ]]; then
+	echo "$word"
+      fi
+    done
+  fi
+  ((count++))
 done 
 
