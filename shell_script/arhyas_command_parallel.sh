7cb3ced51d8979962992899e1fe8a81838bb8ee7
john-washington <52287450+john-washington@users.noreply.github.com>
2025-09-23 20:40:42 +0800

    Update arhyas_command_parallel.sh
    take care of missed target

--- shell_script/arhyas_command_parallel.sh
+++ shell_script/arhyas_command_parallel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
- 
 
+#take care of the target first
+./arhyas_msg.sh "$1"
+
+#traceroute may not show the target, so do the rest secondly
 traceroute "$1" | ./tracelist.sh | xargs -I {} ./arhyas_msg.sh {}
 	
