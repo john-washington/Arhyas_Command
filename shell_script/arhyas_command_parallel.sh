0cf73aed76d6db23969877d68c6868cf711dc11f
john-washington <52287450+john-washington@users.noreply.github.com>
2025-12-06 10:17:22 +0800

    Update arhyas_command_parallel.sh
    force ipv4 on trace

--- shell_script/arhyas_command_parallel.sh
+++ shell_script/arhyas_command_parallel.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {1,1000}
+for i in {1,2000}
 do
    echo "Round ${i}"
    #take care of the target first
    #traceroute may not show the target, so do the rest secondly
-   ./arhyas_msg.sh "$1" && traceroute "$1" | ./tracelist.sh | xargs -I {} ./arhyas_msg.sh {}
+   ./arhyas_msg.sh "$1" && traceroute -4 "$1" | ./tracelist.sh | xargs -I {} ./arhyas_msg.sh {}
    wait $!
    echo "sleep random seconds..."
    sleep $((RANDOM % 360))
