c00c4b04fcd4f3f880cffab4c95704616b96f70d
john-washington <52287450+john-washington@users.noreply.github.com>
2026-01-15 13:16:42 +0800

    new release updates

--- shell_script/arhyas_command_mac.sh
+++ shell_script/arhyas_command_mac.sh
@@ -10,7 +10,7 @@ do
    #take care of the target first
    #traceroute may not show the target, so do the rest secondly
    #./arhyas_msg.sh "$1" && traceroute "$1" | ./tracelist.sh | xargs -I {} ./arhyas_msg.sh {}
-   cat "$1" | xargs -I {} traceroute {} | ./tracelist.sh | xargs -I {} ./arhyas_msg.sh {}
+   cat "$1" | xargs -I {} ./arhyas_msg.sh {} && xargs -I {} traceroute {} | ./tracelist.sh | xargs -I {} ./arhyas_msg.sh {}
    PID=$!
    wait $PID
    SEC=$((RANDOM % 1200))
