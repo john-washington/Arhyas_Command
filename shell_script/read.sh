a8e20b0f0bbf69543d0534a3b68b8ae902406149
john-washington <52287450+john-washington@users.noreply.github.com>
2026-02-13 07:04:42 +0800

    PATH variable added
    executable environment

--- shell_script/read.sh
+++ shell_script/read.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
  
+PATH=$PATH:/opt/local/bin
+export PATH
+
 csv_file="$1"
 host="$2"
  
@@ -7,14 +10,14 @@ str_to_hex() {
     printf '%s' "$1" | od -An -t x1 | tr -d ' \n'
 }
 
-#imeout 15 ping -4 -c 6 $host 
-#status=$?
+/opt/local/bin/timeout 15 ping -c 6 $host 
+status=$?
 
-#if [ $status -eq 124 ]; then
-#        echo "$host is probably unpingable..."
-#elif [ $status -ne 0 ]; then
-#	echo "command failed with status: $status"
-#else
+if [ $status -eq 124 ]; then
+        echo "$host is probably unpingable..."
+elif [ $status -ne 0 ]; then
+	echo "command failed with status: $status"
+else
   while IFS=, read -r field1 field2
   do
     echo "Field 1: $field1"
@@ -24,5 +27,5 @@ str_to_hex() {
     ping -c 6 -p "$hex_string" $host
     sleep $((RANDOM % 30))
   done < "$csv_file"
-#fi
+fi
 exit 0
--- shell_script/timeout.sh
+++ shell_script/timeout.sh
@@ -3,9 +3,10 @@ host="$1"
 code="$2"
 
 #command -v timeout >/dev/null 2>&1 || { echo >&2 "I require timeout but it is not installed. Please install timeout by: port install timeout(mac) or apt install timeout(linux). Aborting."; exit 1; }
+PATH=$PATH:/opt/local/bin
+export PATH
 
-
-/opt/local/bin/timeout 15 ping -c 6 $host
+timeout 15 ping -c 6 $host
 
 status=$?
 if [ $status -eq 124 ]; then
