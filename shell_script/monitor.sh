fda5f3d288b64735be643a7fb5ea4478d3e794cd
john-washington <52287450+john-washington@users.noreply.github.com>
2026-03-22 19:25:14 +0800

    log fix

--- shell_script/monitor.sh
+++ shell_script/monitor.sh
@@ -7,13 +7,18 @@ OS_NAME=$(uname -s)
 
 case "$OS_NAME" in
   Linux*)
+      APP_RES_DIR=~/Arhyas_Command
+      log_dir=../log
       command -v xxd >/dev/null 2>&1 || { echo >&2 "I require xxd but it is not installed. Please install xxd by port install xxd(mac) or apt install xxd(linux). installing..."; sudo apt install xxd; }
+      command -v curl >/dev/null 2>&1 || { echo >&2 "I require curl but it is not installed. Please install curl by port install curl(mac) or apt install curl(linux). installing..."; echo $mypasswd | sudo -S apt install curl; }
       command -v logrotate >/dev/null 2>&1 || { echo >&2 "I require logrotate but it is not installed. Please install logrotate by port install logrotate(mac) or apt install logrotate(linux). installing..."; sudo apt install logrotate; }
       command -v jq >/dev/null 2>&1 || { echo >&2 "I require jq but it is not installed. Please install jq by: port install jp(mac) or apt install jq(linux). installing..."; sudo apt install jq; }
       ;;
   Darwin*)
+      APP_RES_DIR="/Applications/Arhyas Command Multilingual for MacOS 11+.app/Contents/Resources"
+      log_dir="${APP_RES_DIR}/log" 
       command -v xxd >/dev/null 2>&1 || { echo >&2 "I require xxd but it is not installed. Please install xxd by port install xxd(mac) or apt install xxd(linux). installing..."; sudo port install xxd; }
-     
+      command -v curl >/dev/null 2>&1 || { echo >&2 "I require curl but it is not installed. Please install curl by port install curl(mac) or apt install curl(linux). installing..."; echo $mypasswd | sudo -S port install curl; }
       command -v logrotate >/dev/null 2>&1 || { echo >&2 "I require logrotate but it is not installed. Please install logrotate by port install logrotate(mac) or apt install logrotate(linux). installing..."; sudo port install logrotate; }
       command -v jq >/dev/null 2>&1 || { echo >&2 "I require jq but it is not installed. Please install jq by: port install jp(mac) or apt install jq(linux). installing..."; sudo port install jq; }
       ;;
@@ -24,17 +29,18 @@ esac
 
 #exit 1; }
 
-for i in {1..2048}
+for i in {1..4096}
 do
    echo "REFRESH\n"
    ps -ef | grep 'ping' |  ./parse_monitor.sh 
    SEC=$((RANDOM % 12))
-   echo "Round ${i} scheduled, sleeping ${SEC} seconds..."
+  
+   if [[ $((i % 12)) -eq 0 ]]; then
+     echo "checking update..." | tee -a "${log_dir}"/Arhyas_Command.log
+     bash "${APP_RES_DIR}"/update.sh $mypasswd
+   fi
+   echo "Round ${i} scheduled, sleeping ${SEC} seconds..." | tee -a "${log_dir}"/Arhyas_Command.log
    sleep $SEC
 
-   if [[ $((i % 12)) -eq 0 ]]; then
-     echo "checking update..."
-     bash update.sh
-   fi
 done
-echo 'monitor stop, you can restart...'
+echo 'monitor stop, you can restart...' | tee -a "${log_dir}"/Arhyas_Command.log
