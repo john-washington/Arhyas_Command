b1076b26e20c132ebef59e1c5776d42fa399fb52
john-washington <52287450+john-washington@users.noreply.github.com>
2026-04-21 07:43:23 +0800

    Update arhyas_msg.sh

--- shell_script/arhyas_msg.sh
+++ shell_script/arhyas_msg.sh
@@ -17,7 +17,7 @@ case "$OS_NAME" in
   Linux*)
       TEMP_DIR=~/Documents/Arhyas_Command
       APP_RES_DIR=~/Arhyas_Command
-      shell_script="${APP_RES_DIR}"
+      shell_script="${APP_RES_DIR}"/shell_script
       data_dir="${APP_RES_DIR}"/data
       txt_dir="${APP_RES_DIR}"/txt
       log_dir="${APP_RES_DIR}"/log
@@ -58,7 +58,7 @@ found=$(find "${txt_dir}" -type f -name "_ELAi_sa_sequence.csv" )
 if [[ $(wc -c < "$found" ) -eq 0 ]]; then
       echo "${txt_dir}/_ELAi_sa_sequence.csv" "not found"
 else
-      bash read.sh "${txt_dir}/_ELAi_sa_sequence.csv" $host &
+      bash "${shell_script}"/read.sh "${txt_dir}/_ELAi_sa_sequence.csv" $host &
 fi
 
 echo "PROGRESS:10"
@@ -67,7 +67,7 @@ found=$(find "${txt_dir}" -type f -name "_AL_Hum_Bhra_sequence1.csv" )
 if [[ $(wc -c < "$found" ) -eq 0 ]]; then
       echo "${txt_dir}/_AL_Hum_Bhra_sequence1.csv" "not found"
 else
-      bash read.sh "${txt_dir}/_AL_Hum_Bhra_sequence1.csv" $host &
+      bash "${shell_script}"/read.sh "${txt_dir}/_AL_Hum_Bhra_sequence1.csv" $host &
 fi
 
 echo "PROGRESS:20"
@@ -76,7 +76,7 @@ found=$(find "${txt_dir}" -type f -name "_AL_Hum_Bhra_sequence2.csv" )
 if [[ $(wc -c < "$found" ) -eq 0 ]]; then
       echo "${txt_dir}/_AL_Hum_Bhra_sequence2.csv" "not found"
 else
-      bash read.sh "${txt_dir}/_AL_Hum_Bhra_sequence2.csv" $host &
+      bash "${shell_script}"/read.sh "${txt_dir}/_AL_Hum_Bhra_sequence2.csv" $host &
 fi
 
 echo "PROGRESS:30"
@@ -87,7 +87,7 @@ found=$(find "${txt_dir}" -type f -name "_KRP_sequence_beginning.txt" )
 if [[ $(wc -c < "$found" ) -eq 0 ]]; then
       echo "${txt_dir}/_KRP_sequence_beginning.txt" "not found"
 else
-      bash read.sh "${txt_dir}/_KRP_sequence_beginning.txt" $host &
+      bash "${shell_script}"/read.sh "${txt_dir}/_KRP_sequence_beginning.txt" $host &
 fi
 
 echo "PROGRESS:40"
@@ -99,10 +99,10 @@ found=$(find "${txt_dir}" -type f -name "_KRP_multilingual_part-${language_code}
 if [[ $(wc -c < "$found" ) -eq 0 ]]; then
       echo "${txt_dir}/_KRP_multilingual_part-${language_code}-dual.txt" "not found"
 else
-      bash read.sh "${txt_dir}/_KRP_multilingual_part-${language_code}-dual.txt" $host &
+      bash "${shell_script}"/read.sh "${txt_dir}/_KRP_multilingual_part-${language_code}-dual.txt" $host &
 fi
 
-bash read.sh "${txt_dir}/_KRP_ending.txt" $host &
+bash "${shell_script}"/read.sh "${txt_dir}/_KRP_ending.txt" $host &
 
 echo "PROGRESS:50"
 echo "_KRP_Elemental_Command_sequence..."
@@ -118,12 +118,12 @@ found=$(find "${txt_dir}" -type f -name "_KRP_Elemental_Command_sequence-${langu
 if [[ $(wc -c < "$found" ) -eq 0 ]]; then
       echo "${txt_dir}/_KRP_Elemental_Command_sequence-${language_code}-dual.txt"  "not found"
 else
-      bash read.sh "${txt_dir}/_KRP_Elemental_Command_sequence-${language_code}-dual.txt" $host &
+      "${shell_script}"/bash read.sh "${txt_dir}/_KRP_Elemental_Command_sequence-${language_code}-dual.txt" $host &
 fi
 
 echo "PROGRESS:70"
 echo "_12_Strand_DNA_Tribal_Shield_sequence..."
-bash read.sh "${txt_dir}/_12_Strand_DNA_Tribal_Shield_sequence.csv" $host &
+bash "${shell_script}"/read.sh "${txt_dir}/_12_Strand_DNA_Tribal_Shield_sequence.csv" $host &
 
 echo "PROGRESS:80"
 ARHYAS_COMMAND_multilingual="_Arhyas_Command_sequence-${language_code}-dual.txt"
@@ -132,7 +132,7 @@ found=$(find "${txt_dir}" -type f -name "_Arhyas_Command_sequence-${language_cod
 if [[ $(wc -c < "$found" ) -eq 0 ]]; then
       echo "${txt_dir}/_Arhyas_Command_sequence-${language_code}-dual.txt"  "not found"
 else
-      bash read.sh "${txt_dir}/_Arhyas_Command_sequence-${language_code}-dual.txt" $host &
+      bash "${shell_script}"/read.sh "${txt_dir}/_Arhyas_Command_sequence-${language_code}-dual.txt" $host &
 fi
 echo "PROGRESS:100"
 echo "Done"
