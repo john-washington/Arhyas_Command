eaa1a863c2959f79608c702e42cb72e6788ade08
john-washington <52287450+john-washington@users.noreply.github.com>
2026-04-21 06:49:15 +0800

    Update arhyas_msg.sh

--- shell_script/arhyas_msg.sh
+++ shell_script/arhyas_msg.sh
@@ -17,9 +17,10 @@ case "$OS_NAME" in
   Linux*)
       TEMP_DIR=~/Documents/Arhyas_Command
       APP_RES_DIR=~/Arhyas_Command
-      data_dir=../data
-      txt_dir=../txt
-      log_dir=../log
+      shell_script="${APP_RES_DIR}"
+      data_dir="${APP_RES_DIR}"/data
+      txt_dir="${APP_RES_DIR}"/txt
+      log_dir="${APP_RES_DIR}"/log
       command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it is not installed. Please install git by apt install git(linux). installing..."; echo $mypasswd | sudo -S apt install git | tee -a "${APP_RES_DIR}"/arhyas_command.log; }
       command -v logrotate >/dev/null 2>&1 || { echo >&2 "I require logrotate but it is not installed. Please install logrotate by port install logrotate(mac) or apt install logrotate(linux). installing..."; echo $mypasswd | sudo -S apt install logrotate | tee -a "${APP_RES_DIR}"/arhyas_command.log; }
      
@@ -31,6 +32,7 @@ case "$OS_NAME" in
       command -v logrotate >/dev/null 2>&1 || { echo >&2 "I require logrotate but it is not installed. Please install logrotate by port install logrotate(mac) or apt install logrotate(linux). installing..."; echo $mypasswd | sudo -S port install logrotate | tee -a "${APP_RES_DIR}"/arhyas_command.log; }
       
       APP_RES_DIR="/Applications/Arhyas Command Multilingual for MacOS 11+.app/Contents/Resources"
+      shell_script="${APP_RES_DIR}"
       data_dir="${APP_RES_DIR}/data"
       txt_dir="${APP_RES_DIR}"
       log_dir="${APP_RES_DIR}/log"
