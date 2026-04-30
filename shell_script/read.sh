246eaf9d992da721fa8881a96c7e7d2b2001de64
john-washington <52287450+john-washington@users.noreply.github.com>
2026-04-30 19:08:48 +0800

    Update read.sh

--- shell_script/read.sh
+++ shell_script/read.sh
@@ -46,7 +46,7 @@ split_send_sp() {
 
     if [[ $wc_byte -le 16 ]]; then
       ping -c 6 -p "$1" $host | tee -a "${log_dir}/error.log"
-      sudo nmap -Pn -sn --data "$my_hex_string" -sS $host | tee -a "${log_dir}/error.log"
+      sudo nmap -Pn -sn --data "$my_hex_string"  $host | tee -a "${log_dir}/error.log"
            
     else
       
@@ -62,12 +62,12 @@ split_send_sp() {
 
         if [[ $wc_byte2 -le 16 ]]; then
           ping -c 6 -p "$my_hex_string" $host | tee -a "${log_dir}/error.log"
-          sudo nmap -Pn --data "$my_hex_string" -sS $host | tee -a "${log_dir}/error.log"
+          sudo nmap -Pn --data "$my_hex_string"  $host | tee -a "${log_dir}/error.log"
            
         else
            echo "warning: $part encoded to $my_hex_string is too long for transmission, try sending anyway..." | tee -a "${log_dir}/error.log"
            ping -c 6 -p "$my_hex_string" $host | tee -a "${log_dir}/error.log"
-           sudo nmap -Pn --data "$my_hex_string" -sS $host | tee -a "${log_dir}/error.log"
+           sudo nmap -Pn --data "$my_hex_string"  $host | tee -a "${log_dir}/error.log"
            
            #r=$(split_send_hp "$my_hex_string" "$part")
         fi
@@ -85,7 +85,7 @@ split_send_hp() {
 
     if [[ $wc_byte -le 16 ]]; then
       ping -c 6 -p "$1" $host | tee -a "${log_dir}/error.log"
-      sudo nmap -Pn --data "$my_hex_string" -sS $host | tee -a "${log_dir}/error.log"
+      sudo nmap -Pn --data "$my_hex_string"  $host | tee -a "${log_dir}/error.log"
            
     else
       
@@ -101,12 +101,12 @@ split_send_hp() {
 
         if [[ $wc_byte2 -le 16 ]]; then
           ping -c 6 -p "$my_hex_string" $host | tee -a "${log_dir}/error.log"
-          sudo nmap -Pn --data "$my_hex_string" -sS $host | tee -a "${log_dir}/error.log"
+          sudo nmap -Pn --data "$my_hex_string"  $host | tee -a "${log_dir}/error.log"
            
         else
            echo "warning: $part encoded to $my_hex_string is too long for transmission, sending..." | tee -a "${log_dir}/error.log"
            ping -c 6 -p "$my_hex_string" $host | tee -a "${log_dir}/error.log"
-           sudo nmap -Pn --data "$my_hex_string" -sS $host | tee -a "${log_dir}/error.log"
+           sudo nmap -Pn --data "$my_hex_string"  $host | tee -a "${log_dir}/error.log"
            
            #r=$(split_send_sp "$my_hex_string" "$part")
         fi
@@ -123,12 +123,12 @@ status=$?
 if [ $status -eq 124 ]; then
     echo "$host is probably unpingable..." | tee -a "${log_dir}/error.log"
     echo "nmap deep scaning..."
-    sudo nmap -Pn -sn --traceroute -sS $host | tee -a "${log_dir}/error.log"
+    sudo nmap -Pn -sn --traceroute  $host | tee -a "${log_dir}/error.log"
 
 elif [ $status -ne 0 ]; then
   	echo "ping command failed with s#tatus: $status" | tee -a "${log_dir}/error.log"
     echo "nmap deep scaning..."
-    sudo nmap -Pn -sn --traceroute -sS $host | tee -a "${log_dir}/error.log"
+    sudo nmap -Pn -sn --traceroute  $host | tee -a "${log_dir}/error.log"
 fi
 
 #do the sending regardless:
