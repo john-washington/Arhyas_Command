e900ed41b8eb672fb87564ae97c48d87393e0b23
john-washington <52287450+john-washington@users.noreply.github.com>
2026-05-07 08:55:50 +0800

    Update ip-api.sh
    disable psql update for now

--- shell_script/ip-api.sh
+++ shell_script/ip-api.sh
@@ -20,7 +20,7 @@ case "$OS_NAME" in
                 data_dir="${APP_RES_DIR}"/data
                 txt_dir="${APP_RES_DIR}"/txt
                 log_dir="${APP_RES_DIR}"/log
-                config_dir="${}APP_RES_DIR}"/config
+                config_dir="${APP_RES_DIR}"/config
                 ;;
 	Darwin*)
 		command -v timeout >/dev/null 2>&1 || { echo >&2 "I require timeout but it is not installed. Please install timeout by: port install timeout(mac) or apt install timeout(linux). installing..."; sudo port install timeout; }
@@ -28,8 +28,8 @@ case "$OS_NAME" in
 		command -v jq >/dev/null 2>&1 || { echo >&2 "I require jq but it is not installed. Please install jq by: port install jp(mac) or apt install jq(linux). installing..."; sudo port install jq; }
         command -v psql >/dev/null 2>&1 || { echo >&2 "I require psql but it is not installed. Please install psql by: brew install libpq(mac). installing...";  brew install postgresql; }
           
-                #APP_RES_DIR="/Applications/Arhyas Command Multilingual for MacOS 11+.app/Contents/Resources"
-                APP_RES_DIR=~/Arhyas_Command
+                APP_RES_DIR="/Applications/Arhyas Command Multilingual for MacOS 11+.app/Contents/Resources"
+                #APP_RES_DIR=~/Arhyas_Command
                 shell_script="${APP_RES_DIR}"/shell_script
                 data_dir="${APP_RES_DIR}/data"
                 txt_dir="${APP_RES_DIR}"/txt
@@ -76,7 +76,7 @@ while :; do
                
                export PGPASSWORD=xxxx
                
-               psql -h gis.peertalk.net -p 2048 -d osm -U featureserver -w -c "\copy arhyas_command_tracking(track) FROM PROGRAM 'jq -c -r .[] ${inputfile}_geo_data.json'"
+               #psql -h gis.peertalk.net -p 2048 -d osm -U featureserver -w -c "\copy arhyas_command_tracking(track) FROM PROGRAM 'jq -c -r .[] ${inputfile}_geo_data.json'"
 
                cd "${APP_RES_DIR}"
                shift;
@@ -115,7 +115,7 @@ while :; do
                   
                     jq '.[] | .network' "center_${lat}_${lon}_${language_code}_${radius}.json" | tr -d '"' | sed  's/\/[0-9]\{1,\}//g' > "center_${lat}_${lon}_${language_code}_${radius}.txt"
                   
-                    psql -h gis.peertalk.net  -p 2048 -d osm -U featureserver -w -c "\copy circle_search_result_language_coded(result) FROM PROGRAM 'jq -c -r .[] center_${lat}_${lon}_${language_code}_${radius}.json'"
+                    #psql -h gis.peertalk.net  -p 2048 -d osm -U featureserver -w -c "\copy circle_search_result_language_coded(result) FROM PROGRAM 'jq -c -r .[] center_${lat}_${lon}_${language_code}_${radius}.json'"
                     
                     #from here it is differnt than the -s case
                    
@@ -163,7 +163,7 @@ while :; do
                         else
                             pi=${pi_list[${#file_arry[@]}-${i}]}
                         fi  
-                        echo "ssh -t pi@${pi} 'cd ~/Arhyas_Command; tar -czvf data.tar.gz data; rm -rf data ; mkdir -p data; cp ~/${file_arry[$i]} ~/Arhyas_Command/data; ~/Arhyas_Command/shell_script/child_timeout.sh ~/Arhyas_Command/data/${file_arry[$i]}' &"
+                        echo "sshpass -ppi123 ssh -t pi@${pi} 'cd ~/Arhyas_Command; tar -czvf data.tar.gz data; rm -rf data ; mkdir -p data; cp ~/${file_arry[$i]} ~/Arhyas_Command/data; echo 'pi' |  ~/Arhyas_Command/shell_script/child_timeout.sh ~/Arhyas_Command/data/${file_arry[$i]}' &"
                           
                     done > jobs_to_run_2
 
@@ -206,9 +206,9 @@ while :; do
                     
                     jq '.[] | .ip_range_start' "center_${lat}_${lon}_${radius}.json" | tr -d '"' > "center_${lat}_${lon}_${radius}.txt"
                    
-                    export PGPASSWORD=eeZ1tooy
+                    export PGPASSWORD=xxxx
                     
-                    psql -h gis.peertalk.net  -p 2048 -d osm -U featureserver -w -c "\copy circle_search_result(result) FROM PROGRAM 'jq -c -r .[] center_${lat}_${lon}_${radius}.json'"
+                    #psql -h gis.peertalk.net  -p 2048 -d osm -U featureserver -w -c "\copy circle_search_result(result) FROM PROGRAM 'jq -c -r .[] center_${lat}_${lon}_${radius}.json'"
                    
                     command_str2="curl 'http://gis.peertalk.net:9080/functions/public.circle_search_on_centerpoint_${language_code}/items?center_latitude=${lat}&center_longitude=${lon}&radius=${radius}&limit=50000'"
                     echo ${command_str2}
@@ -216,13 +216,14 @@ while :; do
                   
                     jq '.[] | .network' "center_${lat}_${lon}_${language_code}_${radius}.json" | tr -d '"' | sed  's/\/[0-9]\{1,\}//g' > "center_${lat}_${lon}_${language_code}_${radius}.txt"
                   
-                    psql -h gis.peertalk.net  -p 2048 -d osm -U featureserver -w -c "\copy circle_search_result_language_coded(result) FROM PROGRAM 'jq -c -r .[] center_${lat}_${lon}_${language_code}_${radius}.json'"
+                    #psql -h gis.peertalk.net  -p 2048 -d osm -U featureserver -w -c "\copy circle_search_result_language_coded(result) FROM PROGRAM 'jq -c -r .[] center_${lat}_${lon}_${language_code}_${radius}.json'"
             
                     # -s mode for single instance direct execution 
                     cd "${data_dir}"
                     #merge the two 
                     cat center_${lat}_${lon}_${radius}.txt center_${lat}_${lon}_${language_code}_${radius}.txt >> center_${lat}_${lon}_${radius}_merged_list.txt
-                    cat center_${lat}_${lon}_${radius}_merged_list.txt |  bash "${shell_script}"/append_code.sh  "${language_code}" | xargs -n 2  bash "${shell_script}"/timeout.sh 
+                    cat center_${lat}_${lon}_${radius}_merged_list.txt |  bash "${shell_script}"/append_code.sh  "${language_code}" >center_${lat}_${lon}_${radius}_${language_code}_merged_list.txt
+                    cat center_${lat}_${lon}_${radius}_${language_code}_merged_list.txt | xargs -n 2  bash "${shell_script}"/timeout.sh 
                 
                    echo "I am done for -s case"
                 done < "$inputfile"
