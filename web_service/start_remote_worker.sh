be213955f81951dca52fa845f2b35df8947d26da
John S. Zhang <john.washington@peertalk.net>
2026-07-08 17:44:17 +0800

    queue worker run in the background

--- web_service/start_remote_worker.sh
+++ web_service/start_remote_worker.sh
@@ -1,3 +1,3 @@
 source ../flask-env/bin/activate .
-rq worker --logging_level DEBUG  arhyas_queue --url redis://:krystar1@192.168.1.179
+rq worker --logging_level DEBUG  arhyas_queue --url redis://:krystar1@192.168.1.179 &
 
